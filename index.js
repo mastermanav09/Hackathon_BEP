98a15db438bdce49bf7a6f7eceb8e966cf5b7576
Manav Naharwal <manavnaharwalp@gmail.com>
2023-02-25 20:38:16 +0530

    Rephrase the project

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-const express = require("express");
-const { successMessage } = require("./utils/responseUtils");
-const connectMongo = require("./db");
-const cors = require('cors');
-
 require("dotenv").config();
-
+const express = require("express");
 const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoute");
+const matchRoutes = require("./routes/matchRoute");
+const teamRoutes = require("./routes/teamRoute");
+const predictionRoutes = require("./routes/predictionRoute");
+
 const PORT = process.env.PORT;
+const dbUrl = process.env.MONGODB_URI;
+
 app.use(express.json());
 app.use(cors());
 
-app.use(require("./routes/authRoute"));
-app.use(require("./routes/matchRoute"));
-app.use(require("./routes/teamRoute"));
-app.use(require("./routes/predictionRoute"));
+app.use(authRoutes);
+app.use(matchRoutes);
+app.use(teamRoutes);
+app.use(predictionRoutes);
 
-connectMongo();
-
-app.get("/",(req,res)=>{
-    return res.json(successMessage("Welcome To SPL - Sasta Premier League"));
-})
-
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running at Port ", PORT);
-})
+mongoose
+  .connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running at Port ", PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
